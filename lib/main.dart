750d27bd811ed9e76304a564e0c93893036e6c06
Ankit Jailwal <jailwalanki@gmail.com>
2021-03-27 14:42:53 +0530

    Creating Solar App

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:SolarApp/Screens/Welcome/welcome_screen.dart';
+import 'package:SolarApp/constants.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Solar App',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
